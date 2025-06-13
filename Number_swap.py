b7582e19e3a783eeecd9738d45680c86b0b701cf
simmk15 <sknagra15@gmail.com>
2025-06-13 18:56:15 +0530

    Number _swap

---
+++ Number_swap.py
@@ -0,0 +1,11 @@
+a=input("Enter the 1st number=")
+b=input("Enter the 2nd number=")
+c=input("Enter the 3rd number=")
+temp_a=a
+a=b
+temp_b=b
+b=c
+temp_c=c
+c=temp_a
+print("The interchanged values are:")
+print("A=",a,"B=",b,"C=",c)
